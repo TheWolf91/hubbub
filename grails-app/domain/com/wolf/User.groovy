913e2c6bdeb0da5ccee1263d3d6cb80e9d685be4
TheWolf91 <the_wolf91@libero.it>
2017-11-01 17:26:15 +0100

    Set a custom Validator for password field

--- grails-app/domain/com/wolf/User.groovy
+++ grails-app/domain/com/wolf/User.groovy
@@ -9,7 +9,7 @@ class User {
 
     static constraints = {
         loginId size: 3..20, unique: true, nullable: false
-        password size: 6..8, nullable: false
+        password size: 6..8, blank: false, validator: { passwd, user -> passwd != user.loginId }
         homepage url: true, nullable: false
     }
 }
