9a1744c218eb486f8715a43b07652b1a24a88d57
TheWolf91 <the_wolf91@libero.it>
2017-11-03 13:40:48 +0100

    Added Profile, Post, Tag domains and many integration tests

---
+++ grails-app/domain/com/wolf/Post.groovy
@@ -0,0 +1,13 @@
+package com.wolf
+
+class Post {
+    String content
+    Date dateCreated
+
+    static belongsTo = [user : User]
+    static hasMany = [tags : Tag]
+
+    static constraints = {
+        content blank: false
+    }
+}
--- grails-app/domain/com/wolf/User.groovy
+++ grails-app/domain/com/wolf/User.groovy
@@ -3,13 +3,15 @@ package com.wolf
 class User {
     String loginId
     String password
-    String homepage
     Date dateCreated // Naming Date variable as "dateCreated" provides special support: Grails will automatically store the timestamp in it.
     // Date lastUpdated // Like said, also this name provides special support: Grails will automatically store last update time on it.
+    static hasOne = [profile : Profile]
+    static hasMany = [posts : Post, tags : Tag, following: User]
+    static mapping = {sort posts: "desc"}
 
     static constraints = {
         loginId size: 3..20, unique: true, nullable: false
         password size: 6..8, blank: false, validator: { passwd, user -> passwd != user.loginId }
-        homepage url: true, nullable: false
+        profile nullable: true
     }
 }
